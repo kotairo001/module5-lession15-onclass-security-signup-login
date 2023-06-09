188f802339e179e13225b18767d9d73220906dc8
vangtt <giangthanhvan12@gmail.com>
2023-06-09 08:22:21 +0700

    security basic

---
+++ src/main/java/rikkei/academy/sercurity/SecurityInit.java
@@ -0,0 +1,8 @@
+package rikkei.academy.sercurity;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+
+}
